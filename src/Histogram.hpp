dfcba0371194002ce3ff06e8fafc0c499dbf9887
Jakob Schwendner <jakob.schwendner@gmail.com>
2012-12-17 11:12:54 +0100

    added documentation to Histogram class

--- src/Histogram.hpp
+++ src/Histogram.hpp
@@ -16,6 +16,8 @@ struct Buckets
     Buckets( int count, double min_val, double max_val, const T& initial = T() )
 	: count( count ), min_val( min_val ), max_val( max_val ), buckets( count, initial )
     {
+	assert( count > 0 );
+	assert( min_val < max_val );
     }
 
     T& operator[]( size_t idx )
@@ -51,21 +53,55 @@ struct Buckets
     
 };
 
+/**
+ * @brief Histogram class, that splits an interval into n regular bins, and counts how
+ * often a value is added to a bin.
+ *
+ * The counts can be recovered using the bracket operator and a bin index.
+ *
+ * @code
+ * Histogram h( 10, 0, 10 );
+ * h.update( 1.5 );
+ * h.update( 11 );
+ * for( size_t i=0; i<h.size(); i++ )
+ *	cout << h[i] << endl;
+ * @endcode
+ *
+ */
 struct Histogram : public Buckets<size_t>
 {
     size_t n;
 
+    /**
+     * @brief Constructs a histogram where the number of update() calls with a value
+     * that all into a bin are counted.
+     *
+     * @param count - number of divisions in the interval, needs to be greater 0
+     * @param min_val - lower bound for the interval 
+     * @param max_val - upper bound for the interval, needs to be greater than
+     *			min_val
+     */
     Histogram( int count, double min_val, double max_val )
 	: Buckets<size_t>( count, min_val, max_val )
     {
     }
 
+    /**
+     * Increase the count for the bin, which value fits in by one.  This
+     * function will assign a value which is less than min_val the first bucket,
+     * and greater than max_val to the last bucket.
+     *
+     * @param value - value for which the count in the bin should be increased
+     */
     void update( double value )
     {
 	++n;
 	++get( value );
     }
 
+    /**
+     * total number of values in the bins
+     */
     size_t total() const
     {
 	return n;
