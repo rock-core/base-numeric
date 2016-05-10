a842c9ddc4b25c44b6f965124ad35b5c13efe227
Joao Neto <joao.neto@fieb.org.br>
2016-05-10 12:49:19 +0200

    Include Savitzky Golay filter. Used for smooth and differentiation.

---
+++ src/SavitzkyGolayFilter.hpp
@@ -0,0 +1,78 @@
+#ifndef __NUMERIC_SAVITZKY_GOLAY_FILTER_HPP__
+#define __NUMERIC_SAVITZKY_GOLAY_FILTER_HPP__
+
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+
+namespace numeric
+{
+/**
+ * Compute coefficients of the Savitzky-Golay filter.
+ *
+ * Based on:
+ * General Least-Squares smoothing and differentiation by the Convolution (Savitzky-Golay) method.
+ * By Peter A. Gory (1989)
+ */
+class SavitzkyGolayFilter
+{
+public:
+
+    /** Compute the coefficients of Savitzky-Golay filter
+     *
+     *  @param result coefficients
+     *  @param Least-Square point, between [-half_width, half_width]
+     *  @param half width
+     *  @param polynomial order
+     *  @param derivative. 0 = smooth
+     *  @param step. Necessary in case of derivatives, when the coefficients need to be divided by the step.
+     */
+    SavitzkyGolayFilter(std::vector<double>& result, int ls_point, int half_width, int poly_order, int derivative = 0, double step = 1);
+    ~SavitzkyGolayFilter();
+
+private:
+
+    /** Calculates the weight of the i'th data point for the t'th least-Square point of the s'th derivative, over 2m+1 points, order n
+     *
+     *  @param i data point, between [-m, m]
+     *  @param t Least-Square point, between [-m, m]
+     *  @param m half width
+     *  @param n polynomial order
+     *  @param s derivative
+     *  @return weight
+     */
+    double weight(int i, int t, int m, int n, int s);
+
+    /** Calculates the Gram Polynomial (s=0), or its s'th derivative evaluated at i, order k, over 2m+1 points
+     *
+     *  @param i data point
+     *  @param m half width
+     *  @param k order
+     *  @param s derivative
+     *  @return gram polynomial, or it's derivative
+     */
+    double gramPolynomial(int i, int m, int k, int s);
+
+    /** Calculates the generalized factorial (a)(a-1)...(a-b+1)
+     *
+     *  @param a higher number
+     *  @param b lower limit
+     *  @return (a)(a-1)...(a-b+1)
+     */
+    double generalizedFactorial(int a, int b);
+
+    /** Chack parameters of Savitzky-Golay filter
+     *
+     *  @param t Least-Square point
+     *  @param m half width
+     *  @param n polynomial order
+     *  @param s derivative
+     */
+    void checkParameters(int t, int m, int n, int s, double step);
+};
+}
+
+#endif
---
+++ test/test_SavitzkyGolayFilter.cpp
@@ -0,0 +1,278 @@
+#define BOOST_TEST_MODULE TEST_SAVITZKY_GOLAY_FILTER
+#include <boost/test/included/unit_test.hpp>
+#include <numeric/SavitzkyGolayFilter.hpp>
+#include <iostream>
+
+//test if Savitzky-Golay filter coefficients generator is working
+BOOST_AUTO_TEST_CASE(test_savitzky_golay_5pt_smooth_quadratic)
+{
+    /**
+     * From: General Least-Squares smoothing and differentiation by the Convolution (Savitzky-Golay) method.
+     *  5 pt, quadratic, smooth
+        -------------------------
+        i   -2  -1  0   1   2
+
+        -2  31  9   -3  -5  3
+        -1  9   13  12   6  -5
+        0   -3  12  17  12  -3
+        1   -5  6   12  13   9
+        2   3   -5  -3  9   31
+    norm    35  35  35  35  35
+     */
+    int half_width = 2;
+    int polynom_order = 2;
+    std::vector<double> values;
+    std::vector<double> coeff;
+
+    //// ls-point = -2
+    values.push_back(31);
+    values.push_back(9);
+    values.push_back(-3);
+    values.push_back(-5);
+    values.push_back(3);
+    std::transform(values.begin(), values.end(), values.begin(), std::bind2nd(std::divides<double>(), 35));
+
+    int ls_point = -2;
+    numeric::SavitzkyGolayFilter(coeff, ls_point, half_width, polynom_order);
+
+    for(int i = -half_width; i <= half_width; i++)
+        BOOST_CHECK_CLOSE(coeff[i+half_width], values[i+half_width], 0.0001);
+
+    //// ls-point = -1
+    values.clear();
+    values.push_back(9);
+    values.push_back(13);
+    values.push_back(12);
+    values.push_back(6);
+    values.push_back(-5);
+    std::transform(values.begin(), values.end(), values.begin(), std::bind2nd(std::divides<double>(), 35));
+
+    ls_point = -1;
+    numeric::SavitzkyGolayFilter(coeff, ls_point, half_width, polynom_order);
+    for(int i = -half_width; i <= half_width; i++)
+        BOOST_CHECK_CLOSE(coeff[i+half_width], values[i+half_width], 0.0001);
+
+    //// ls-point = 0
+    values.clear();
+    values.push_back(-3);
+    values.push_back(12);
+    values.push_back(17);
+    values.push_back(12);
+    values.push_back(-3);
+    std::transform(values.begin(), values.end(), values.begin(), std::bind2nd(std::divides<double>(), 35));
+
+    ls_point = 0;
+    numeric::SavitzkyGolayFilter(coeff, ls_point, half_width, polynom_order);
+    for(int i = -half_width; i <= half_width; i++)
+        BOOST_CHECK_CLOSE(coeff[i+half_width], values[i+half_width], 0.0001);
+
+    //// ls-point = 1
+    values.clear();
+    values.push_back(-5);
+    values.push_back(6);
+    values.push_back(12);
+    values.push_back(13);
+    values.push_back(9);
+    std::transform(values.begin(), values.end(), values.begin(), std::bind2nd(std::divides<double>(), 35));
+
+    ls_point = 1;
+    numeric::SavitzkyGolayFilter(coeff, ls_point, half_width, polynom_order);
+    for(int i = -half_width; i <= half_width; i++)
+        BOOST_CHECK_CLOSE(coeff[i+half_width], values[i+half_width], 0.0001);
+
+    //// ls-point = 2
+    values.clear();
+    values.push_back(3);
+    values.push_back(-5);
+    values.push_back(-3);
+    values.push_back(9);
+    values.push_back(31);
+    std::transform(values.begin(), values.end(), values.begin(), std::bind2nd(std::divides<double>(), 35));
+
+    ls_point = 2;
+    numeric::SavitzkyGolayFilter(coeff, ls_point, half_width, polynom_order);
+    for(int i = -half_width; i <= half_width; i++)
+        BOOST_CHECK_CLOSE(coeff[i+half_width], values[i+half_width], 0.0001);
+}
+
+BOOST_AUTO_TEST_CASE(test_savitzky_golay_7pt_1stderivative_quadratic)
+{
+    /**
+     * From: General Least-Squares smoothing and differentiation by the Convolution (Savitzky-Golay) method.
+     *  7 pt, quadratic, 1st derivative
+        -------------------------
+        i   -3  -2  -1  0   1   2   3
+
+        -3  -13 -29 -19 -3  1   11  7
+        -2  -2  -6  -6  -2  -6  -6  -2
+        -1  5   9   3   -1  -9  -15 -7
+        0   8   16  8   0   -8  -16 -8
+        1   7   15  9   1   -3  -9  -5
+        2   2   6   6   2   6   6   2
+        3   -7  -11 -1  3   19  29  13
+    norm    28  84  84  28  84  84  28
+     */
+    std::vector<double> values;
+    std::vector<double> coeff;
+    int half_width = 3;
+    int polynom_order = 2;
+    int derivative = 1;
+
+    //// ls-point = -3
+    values.push_back(-13);
+    values.push_back(-2);
+    values.push_back(5);
+    values.push_back(8);
+    values.push_back(7);
+    values.push_back(2);
+    values.push_back(-7);
+    std::transform(values.begin(), values.end(), values.begin(), std::bind2nd(std::divides<double>(), 28));
+
+    int ls_point = -3;
+    numeric::SavitzkyGolayFilter(coeff, ls_point, half_width, polynom_order, derivative);
+
+    for(int i = -half_width; i <= half_width; i++)
+        BOOST_CHECK_CLOSE(coeff[i+half_width], values[i+half_width], 0.0001);
+
+    //// ls-point = -2
+    values.clear();
+    values.push_back(-29);
+    values.push_back(-6);
+    values.push_back(9);
+    values.push_back(16);
+    values.push_back(15);
+    values.push_back(6);
+    values.push_back(-11);
+    std::transform(values.begin(), values.end(), values.begin(), std::bind2nd(std::divides<double>(), 84));
+
+    ls_point = -2;
+    numeric::SavitzkyGolayFilter(coeff, ls_point, half_width, polynom_order, derivative);
+    for(int i = -half_width; i <= half_width; i++)
+        BOOST_CHECK_CLOSE(coeff[i+half_width], values[i+half_width], 0.0001);
+
+    //// ls-point = -1
+    values.clear();
+    values.push_back(-19);
+    values.push_back(-6);
+    values.push_back(3);
+    values.push_back(8);
+    values.push_back(9);
+    values.push_back(6);
+    values.push_back(-1);
+    std::transform(values.begin(), values.end(), values.begin(), std::bind2nd(std::divides<double>(), 84));
+
+    ls_point = -1;
+    numeric::SavitzkyGolayFilter(coeff, ls_point, half_width, polynom_order, derivative);
+    for(int i = -half_width; i <= half_width; i++)
+        BOOST_CHECK_CLOSE(coeff[i+half_width], values[i+half_width], 0.0001);
+
+    //// ls-point = 0
+    values.clear();
+    values.push_back(-3);
+    values.push_back(-2);
+    values.push_back(-1);
+    values.push_back(0);
+    values.push_back(1);
+    values.push_back(2);
+    values.push_back(3);
+    std::transform(values.begin(), values.end(), values.begin(), std::bind2nd(std::divides<double>(), 28));
+
+    ls_point = 0;
+    numeric::SavitzkyGolayFilter(coeff, ls_point, half_width, polynom_order, derivative);
+    for(int i = -half_width; i <= half_width; i++)
+        BOOST_CHECK_CLOSE(coeff[i+half_width], values[i+half_width], 0.0001);
+
+    //// ls-point = 1
+    values.clear();
+    values.push_back(1);
+    values.push_back(-6);
+    values.push_back(-9);
+    values.push_back(-8);
+    values.push_back(-3);
+    values.push_back(6);
+    values.push_back(19);
+    std::transform(values.begin(), values.end(), values.begin(), std::bind2nd(std::divides<double>(), 84));
+
+    ls_point = 1;
+    numeric::SavitzkyGolayFilter(coeff, ls_point, half_width, polynom_order, derivative);
+    for(int i = -half_width; i <= half_width; i++)
+        BOOST_CHECK_CLOSE(coeff[i+half_width], values[i+half_width], 0.0001);
+
+    //// ls-point = 2
+    values.clear();
+    values.push_back(11);
+    values.push_back(-6);
+    values.push_back(-15);
+    values.push_back(-16);
+    values.push_back(-9);
+    values.push_back(6);
+    values.push_back(29);
+    std::transform(values.begin(), values.end(), values.begin(), std::bind2nd(std::divides<double>(), 84));
+
+    ls_point = 2;
+    numeric::SavitzkyGolayFilter(coeff, ls_point, half_width, polynom_order, derivative);
+    for(int i = -half_width; i <= half_width; i++)
+        BOOST_CHECK_CLOSE(coeff[i+half_width], values[i+half_width], 0.0001);
+
+    //// ls-point = 3
+    values.clear();
+    values.push_back(7);
+    values.push_back(-2);
+    values.push_back(-7);
+    values.push_back(-8);
+    values.push_back(-5);
+    values.push_back(2);
+    values.push_back(13);
+    std::transform(values.begin(), values.end(), values.begin(), std::bind2nd(std::divides<double>(), 28));
+
+    ls_point = 3;
+    double step = 0.01;
+    numeric::SavitzkyGolayFilter(coeff, ls_point, half_width, polynom_order, derivative, step);
+    for(int i = -half_width; i <= half_width; i++)
+        BOOST_CHECK_CLOSE(coeff[i+half_width]*step, values[i+half_width], 0.0001);
+}
+
+BOOST_AUTO_TEST_CASE(test_savitzky_golay_9pt_3stderivative_sextic)
+{
+    /**
+     * From wikipedia: https://en.wikipedia.org/wiki/Savitzky%E2%80%93Golay_filter
+     *  9 pt, sextic, 3st derivative
+        -------------------------
+        i       0
+
+        -4      100
+        -3      -457
+        -2      256
+        -1      459
+        0       0
+        1       -459
+        2       -256
+        3       457
+        4       -100
+    norm        1144
+     */
+
+    std::vector<double> values;
+    std::vector<double> coeff;
+    int half_width = 4;
+    int polynom_order = 6;
+    int derivative = 3;
+    int ls_point = 0;
+
+    //// ls-point = 0
+    values.push_back(100);
+    values.push_back(-457);
+    values.push_back(256);
+    values.push_back(459);
+    values.push_back(0);
+    values.push_back(-459);
+    values.push_back(-256);
+    values.push_back(457);
+    values.push_back(-100);
+    std::transform(values.begin(), values.end(), values.begin(), std::bind2nd(std::divides<double>(), 1144));
+
+    numeric::SavitzkyGolayFilter(coeff, ls_point, half_width, polynom_order, derivative);
+
+    for(int i = -half_width; i <= half_width; i++)
+        BOOST_CHECK_CLOSE(coeff[i+half_width], values[i+half_width], 0.0001);
+}
